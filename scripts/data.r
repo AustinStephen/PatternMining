b6e7ee54dd901edf917c6415148c281466b582ab
AustinStephen <austinstephen98@gmail.com>
2022-04-13 16:50:14 -0600

    Added more data

--- scripts/data.r
+++ scripts/data.r
@@ -9,7 +9,9 @@ data <- read.csv("data/SMP500.csv")
 ## discretize the data, date management 
 data <- data %>% mutate(
     date = as.Date(Date,format ="%Y-%d-%m"),
-    dateNumeric = as.numeric(date),
+    dateNumeric = as.numeric(date) + 36159,
+    dateNumeric2 = dateNumeric**2,
+    dateNumeric3 = dateNumeric**3,
     discrRes1 = as.factor(round(SP500, digits = 0)),
     discrRes2 = as.factor(round(SP500/10, digits = 0) *10),
     discrRes3 = as.factor(round(SP500/100, digits = 0) *100),
@@ -19,15 +21,94 @@ data <- data %>% mutate(
     discrRes7 = as.factor(round(SP500/1000, digits = 0) * 1000)
     ) %>%
   select(-Date)
+#summary(data)
 
-summary(data)
-## getting the residuals
-model <- lm(SP500 ~ dateNumeric, data=data)
+## building models to look at the residuals
+mLinear <- lm(SP500 ~ dateNumeric, data=data)
 
-summary(model)
+mQuad <- lm(SP500 ~ dateNumeric + dateNumeric2, data=data)  
 
-data$residuals <- model$residuals
+mCube <- lm(SP500 ~ dateNumeric + dateNumeric2 + dateNumeric3, data=data)  
 
-write.csv(data,"data/SMP500_mod.csv")
+data$residualsLinear <- mLinear$residuals
+data$residualsQuad <- mQuad$residuals
+data$residualsCube <- mCube$residuals
 
-tmp<- read.csv("data/SMP500_mod.csv")
+# plotting the data and looking at the residuals
+data %>% ggplot(aes(x=dateNumeric, y=SP500))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+data %>% ggplot(aes(x=dateNumeric, y=residualsLinear))+
+  geom_point()+
+  geom_smooth(method = )
+
+data %>% ggplot(aes(x=dateNumeric, y=residualsQuad))+
+  geom_point()+
+  geom_smooth()
+
+data %>% ggplot(aes(x=dateNumeric, y=residualsCube))+
+  geom_point()+
+  geom_smooth()
+
+dataWrite <- data %>% select(-c(dateNumeric2,dateNumeric3))
+## writting the data
+write.csv(dataWrite,"data/SMP500_mod.csv", row.names = FALSE)
+#tmp<- read.csv("data/SMP500_mod.csv")
+
+
+# Repeating everything for post 1900 --------------------------------------
+
+# remove observations pre 1900
+data_1900 <- data %>% filter( dateNumeric > 10592)
+
+
+# building models to get the residuals
+mLinear <- lm(SP500 ~ dateNumeric, data=data_1900)
+
+mQuad <- lm(SP500 ~ dateNumeric + dateNumeric2, data=data_1900)  
+
+mCube <- lm(SP500 ~ dateNumeric + dateNumeric2 + dateNumeric3, data=data_1900)  
+
+# summary(mLinear)
+# summary(mQuad)
+# summary(mCube)
+
+data_1900$residualsLinear <- mLinear$residuals
+data_1900$residualsQuad <- mQuad$residuals
+data_1900$residualsCube <- mCube$residuals
+
+# removing junk columns
+dataWrite <-data_1900 %>% select(-c(dateNumeric2,dateNumeric3))
+
+## writting the data
+write.csv(dataWrite,"data/SMP500_post_1900.csv", row.names = FALSE)
+
+# Repeating everything from 1950 on ---------------------------------------
+# Repeating everything for post 1900 --------------------------------------
+
+# remove observations pre 1900
+data_1950 <- data %>% filter( dateNumeric > 28854)
+
+
+# building models to get the residuals
+mLinear <- lm(SP500 ~ dateNumeric, data=data_1950)
+
+mQuad <- lm(SP500 ~ dateNumeric + dateNumeric2, data=data_1950)  
+
+mCube <- lm(SP500 ~ dateNumeric + dateNumeric2 + dateNumeric3, data=data_1950)  
+
+# summary(mLinear)
+# summary(mQuad)
+# summary(mCube)
+
+data_1950$residualsLinear <- mLinear$residuals
+data_1950$residualsQuad <- mQuad$residuals
+data_1950$residualsCube <- mCube$residuals
+
+# removing junk columns
+dataWrite <-data_1950 %>% select(-c(dateNumeric2,dateNumeric3))
+
+## writting the data
+write.csv(dataWrite,"data/SMP500_post_1950.csv", row.names = FALSE)
+
